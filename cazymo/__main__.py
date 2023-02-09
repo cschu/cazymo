07a80cd362b1571d9c4f7615f360a9b322ace51b
Christian Schudoma <cschu1981@gmail.com>
2023-02-09 20:57:16 +0100

    fix: call read_count by proper name

--- cazymo/__main__.py
+++ cazymo/__main__.py
@@ -105,7 +105,7 @@ def main():
                     ("samtools", "view", "-F 4", "-Sh", "-",), stdin=bwa_proc.stdout, stdout=subprocess.PIPE
                 )
                 read_count_proc = subprocess.Popen(
-                    ("gq_read_count",), stdin=samtools_filter_proc, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+                    ("read_count",), stdin=samtools_filter_proc, stdout=subprocess.PIPE, stderr=subprocess.PIPE
                 )
                 samtools_convert_proc = subprocess.Popen(
                     ("samtools", "view", "-buSh", "-",), stdin=read_count_proc.stdout, stdout=subprocess.PIPE
